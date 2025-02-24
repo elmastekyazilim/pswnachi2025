299feb53b07b183353dc5eebb23f07a11a803f9f
hakan <elmastekyazilim@gmail.com>
2025-02-24 19:41:39 +0300

    30 Commit

--- api/process-password.js
+++ api/process-password.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
